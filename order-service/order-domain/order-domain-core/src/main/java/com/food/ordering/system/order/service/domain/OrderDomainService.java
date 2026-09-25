0269aa45255df3591f8f37964be98be3fb53fb46
Hunterkilltree <snguoila@yahoo.com>
2026-09-25 18:52:09 +0700

    Add OrderDomainService interface

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainService.java
@@ -0,0 +1,30 @@
+package com.food.ordering.system.order.service.domain;
+
+
+/**
+ * Return domain event form the domain service
+ * This means the event firing process will be on the called service
+ * which will be on the caller service 
+ * The events will still created in the domain core either in entity or in domain service
+ * However, the event firing will be in the application service
+ * Because before firing an event, the underlying business operation
+ * should be persisted in db
+ * Why don't fire event on domain service or entity? I need to persist the business logic results
+ * However, like to deligate to the application service to prevent unnessary job form domain core module
+ * which should only focus on business
+ * Where to fire the event? In the application service. Domain layer should not know about how to fire the event.
+ * Where to create the event? Domain Service or Entities
+ */
+
+public interface OrderDomainService {
+
+    OrderCreatedEvent validateInitialOrder(Order order, Restaurant restaurant);
+
+    OrderPaidEvent payOrder(Order order);
+
+    void approveOrder(Order order);
+
+    OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages);
+
+    void cancelOrder(Order order, List<String> failureMessages);
+}
